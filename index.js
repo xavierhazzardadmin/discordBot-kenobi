3c070b90263df4672943d20e2a71553b95cff445
xavierhazzardadmin <xavier@hazzard.org.uk>
2020-06-08 21:09:12 +0100

    Added Procfile

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+var fs = require("fs");
+const Discord = require("discord.js");
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const TOKEN = process.env.token;
+const app = require("express")();
 
+app.get("/", async function (req, res) {
+	res.setHeader("content-type", "text/html");
+	res.end(fs.readFileSync("index.html"));
+});
+
+app.listen(3000);
 bot.login(TOKEN);
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+bot.on("ready", () => {
+	console.clear();
+	console.info(
+		`Logged in as \u001b[36m${bot.user.tag.replace(
+			"#",
+			"\u001b[38;5;208m#"
+		)}\u001b[0m!`
+	);
+	bot.user.setActivity("STAR WARS", { type: "WATCHING" });
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+bot.on("message", (msg) => {
+	if (msg.content === "ping") {
+		msg.reply("pong");
+	}
+	if (msg.content.match(/kenobi/gi)) {
+		msg.reply("Hello There");
+	}
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
-    }
-  }
+	if (msg.content.toLowerCase().replace(/[^\w ]/g, "") == "i hate you") {
+		msg.reply("You were my brother Anakin, I loved you");
+	} else if (
+		msg.content.toLowerCase().replace(/[^\w ]/g, "") ===
+		"if youre not with me then youre my enemy"
+	) {
+		msg.reply("Only a sith deals in absolutes, I will do what I must!");
+	} else if (msg.content.match(/absolutely/gi)) {
+		msg.reply("Only a sith deals in absolutes, I will do what i must");
+
+		setTimeout(() => {
+			msg.channel.reply("***Ignites Lightsaber***");
+		}, 1750);
+	} else if (msg.content === "runClicker") {
+		for (let i = 0; i < 120; i++) {
+			msg.channel.send("/click");
+		}
+	}
+	// } else {
+	// 	msg.reply("Please tag a valid user!");
+	// }
 });
+undefined;
